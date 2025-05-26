6b1baff5668b171fc6a55b1f8bf1321e87dffa0a
Hélène Rizzon <helenerizzon3@gmail.com>
2025-05-26 11:04:00 +0200

    méthode .map avec useEffect

--- components/TestComposant.tsx
+++ components/TestComposant.tsx
@@ -2,31 +2,20 @@ import {Text, Pressable} from 'react-native';
 import {useEffect, useState} from "react";
 
 
-// type TestComposantProps = {value: string, callback: (event : GestureResponderEvent) => void}
-
-
 export function TestComposant() {
 
-  const [nombreClics, setNombreClics] = useState(0)
+  const [nombreClics, setNombreClics] = useState(99)
 
-  let jsxlisteUtilisateurs : any;
+  const [jsxlisteUtilisateurs, setJsxlisteUtilisateurs] = useState([]);
 
   useEffect(() => { // qui va déclencher qu'une seule fois la méthode suivante
     fetch("https://jsonplaceholder.typicode.com/users")// requete sur un serveur - pour l'instant sur un faux svr
         .then(response => response.json()) // va nous renvoyer un json
-        .then(utilisateurs => jsxlisteUtilisateurs = utilisateurs.map(  // avec méthode map -> key obligatoire
+        .then(utilisateurs => setJsxlisteUtilisateurs = utilisateurs.map(  // avec méthode map -> key obligatoire
             (utilisateur : {name : string})  => (<Text key={utilisateur.name}>{utilisateur.name}</Text>) // objet 'utilisateur' avec une propriété name de type string
         ))
-  }, [nombreClics]);
+  }, []);
 
-  // 1 ère technique montrée avec .map()
-
-  // const listeUtilisateurs = [{nom: "tata"}, {nom: "titi"}, {nom: "toto"}]
-  //                                           // ["tata", "titi", "toto"]
-  // const listeNomUtilisateurs = listeUtilisateurs.map(
-  //     utilisateur => utilisateur.nom // il y a un return implicite après la =>
-  // )
-  // console.log(listeNomUtilisateurs)
 
   return <>
     <Pressable onPress={() => {
